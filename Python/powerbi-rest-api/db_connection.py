a05b2a6a780eb85507bdd377363560f476d8a00f
nataliaarhus <142580851+nataliaarhus@users.noreply.github.com>
2024-04-08 13:47:25 +0200

    Update db_connection.py

--- Python/powerbi-rest-api/db_connection.py
+++ Python/powerbi-rest-api/db_connection.py
@@ -1,16 +1,15 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import po_constants as poc
 import secret_manager_utils as smu
 
 def connect_db(environment_name):
     env_name = environment_name.lower()
     if env_name == 'production':
-        credentials = smu.get_db_credentials(poc.secret_manager_mapping[""]) # add prod secret name
+        credentials = smu.get_db_credentials('') # add prod secret name
     elif env_name == 'preprod':
-        credentials = smu.get_db_credentials(poc.secret_manager_mapping[""]) # add preprod secret name
+        credentials = smu.get_db_credentials('') # add preprod secret name
     else:
-        credentials = smu.get_db_credentials(poc.secret_manager_mapping[""]) # add staging secret name
+        credentials = smu.get_db_credentials('') # add staging secret name
     try:
         conn = psycopg2.connect(**credentials)
         conn.autocommit = True  # auto-commit each entry to the database
